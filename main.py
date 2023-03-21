bc826b1de59f469a22446d24168c9d93efb9cdff
angel <a.peter@ufl.edu>
2023-03-21 15:06:05 -0400

    added the decode function #angel

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def encode_pass(old_pass):
         return new_pass
 
 
+# added the decode function
+def decode(new_pass):
+    decode_pass = ''
+    decode_dict = {"0": "7", "1": "8", "2": "9", "3": "0", "4": "1",
+                        "5": "2", "6": "3", "7": "4", "8": "5", "9": "6"}
+    for i in new_pass:
+        decode_pass += decode_dict[i]
+    return decode_pass
+
 
 if __name__ == "__main__":
     while True:
@@ -22,7 +31,7 @@ if __name__ == "__main__":
             new_password = encode_pass(original_pass)
             print('Your password has been encoded and stored!\n')
         if choice == 2:
-            decoded_pass = decode_pass(new_password)
+            decoded_pass = decode(new_password)
             print(f'The encoded password is {new_password}, and the original password is {decoded_pass}.\n')
         if choice == 3:
             break
